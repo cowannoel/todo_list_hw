5a0102bb96dbbba904bacd33b04c7d92fed2f2e6
cowannoel <cowannoel@gmail.com>
2020-02-24 16:47:53 +0000

    mvp complete

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+      todos: ["Buy shopping", "Clean bathroom", "Car's MOT"
+    ],
+      newTodo: ""
+
+    },
+    methods: {
+      saveNewTodo: function(){
+        this.todos.push(this.newTodo);
+        this.newTodo = "";
+      }
+    }
+  })
+
+
+
+
+
+});
